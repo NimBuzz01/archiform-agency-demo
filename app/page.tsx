d5c0f7f31b52ef486b28663b04fa194ae3ccf5e1
niamatmarjan <86093827+NimBuzz01@users.noreply.github.com>
2024-05-27 20:53:27 +0530

    add loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
+"use client";
+import Loader from "@/components/Loader";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      setTimeout(() => {
+        setLoading(false);
+        document.body.style.cursor = "default";
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
+  }, []);
+
   return (
-    <main className="sm:container">
-      <Header />
-      <Hero />
+    <main className="min-h-screen">
+      <AnimatePresence mode="wait">
+        {loading && <Loader key="loader" />}
+        <main className="sm:container">
+          <Header />
+          <Hero />
+        </main>
+      </AnimatePresence>
     </main>
   );
 }
