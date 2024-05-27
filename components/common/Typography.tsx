4f2054de6eeb60e2d8420aba6725975a930b9179
niamatmarjan <86093827+NimBuzz01@users.noreply.github.com>
2024-05-27 20:33:18 +0530

    add typography component

---
+++ components/common/Typography.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+import React from "react";
+
+const Typography = ({
+  text,
+  className,
+}: {
+  text: string;
+  className?: string;
+}) => {
+  const words = text.split(" ");
+
+  const containerVariants = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.005,
+      },
+    },
+  };
+
+  const letterVariants = {
+    hidden: { y: "100%" },
+    visible: {
+      y: "0%",
+      transition: { duration: 0.5 },
+    },
+  };
+
+  return (
+    <div
+      className={cn(
+        "relative inline-flex max-w-lg text-base sm:text-lg lg:text-xl font-medium tracking-wide",
+        className
+      )}
+    >
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ margin: "-50px", once: true }}
+        className="flex flex-wrap"
+      >
+        {words.map((word, wordIndex) => (
+          <div key={wordIndex} className="flex overflow-hidden">
+            {word.split("").map((letter, letterIndex) => (
+              <motion.span
+                key={`${wordIndex}-${letterIndex}`}
+                variants={letterVariants}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            <motion.span variants={letterVariants}>&nbsp;</motion.span>
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
+};
+
+export default Typography;
