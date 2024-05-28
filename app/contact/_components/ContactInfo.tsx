4af370ec9e742d3e46c652c6cfb89029144074ef
niamatmarjan <86093827+NimBuzz01@users.noreply.github.com>
2024-05-28 22:15:45 +0530

    add animations to contact info

--- app/contact/_components/ContactInfo.tsx
+++ app/contact/_components/ContactInfo.tsx
@@ -1,3 +1,4 @@
+import Typography from "@/components/common/Typography";
 import PageLink from "@/components/hero/components/PageLink";
 import { socialLinks } from "@/lib/data";
 import React from "react";
@@ -5,15 +6,15 @@ import React from "react";
 const ContactInfo = () => {
   return (
     <div className="flex flex-col gap-10">
-      <div className="text-sm sm:text-base md:text-lg">
-        <p className="font-bold mb-6">Let&apos;s Talk</p>
-        <p>hello@archiformagency</p>
-        <p>+94 123 456 789</p>
+      <div className="text-sm sm:text-base md:text-lg flex flex-col gap-1">
+        <p className="font-bold mb-5">Let&apos;s Talk</p>
+        <Typography text="hello@archiformagency" />
+        <Typography text="+94 123 456 789" />
       </div>
-      <div className="text-sm sm:text-base md:text-lg">
-        <p className="font-bold mb-6">Visit</p>
-        <p>123, Grand Avenue</p>
-        <p>London, United Kingdom</p>
+      <div className="text-sm sm:text-base md:text-lg flex flex-col gap-1">
+        <p className="font-bold mb-5">Visit</p>
+        <Typography text="123, Grand Avenue" />
+        <Typography text="London, United Kingdom" />
       </div>
       <div className="flex flex-col text-sm sm:text-base gap-2 md:text-lg">
         <p className="font-bold mb-6">Socials</p>
