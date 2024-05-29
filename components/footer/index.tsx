ccf69919e742231de38f11b9ee1b1babec3aef5d
niamatmarjan <86093827+NimBuzz01@users.noreply.github.com>
2024-05-29 14:31:09 +0530

    fix footer alignment

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
       className="bg-cmprimary flex flex-col items-center text-cmsecondary w-full relative h-[600px]"
       style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
     >
-      <div className="fixed px-3 py-10 sm:py-16 sm:container bottom-0 w-full h-[600px]">
+      <div className="fixed px-3 py-10 sm:container bottom-0 w-full flex flex-col h-[600px]">
         <div className="flex flex-wrap gap-10">
           <div className="text-sm">
             <p className="mb-6 text-base font-bold">Let&apos;s Talk</p>
@@ -35,7 +35,7 @@ const Footer = () => {
             ))}
           </div>
         </div>
-        <div className="mt-20 sm:mt-32">
+        <div className="mt-auto">
           <Title
             text="Archiform"
             stagger={0.05}
@@ -49,11 +49,11 @@ const Footer = () => {
             className="text-4xl font-light sm:text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl"
           />
         </div>
-        <div className="flex flex-col gap-1 mt-10 mb-4 sm:justify-between sm:items-center sm:flex-row">
+        <div className="flex flex-col gap-4 mt-10 mb-4 sm:justify-between sm:items-center sm:flex-row">
           <div className="flex items-center gap-1">
             <CopyrightIcon className="w-4 h-4" /> 2024 Archiform Agency
           </div>
-          <div className="flex items-center gap-4 text-sm uppercase">
+          <div className="flex items-center gap-4 text-xs uppercase sm:text-sm">
             {socialLinks.map((link) => (
               <PageLink
                 text={link.title}
