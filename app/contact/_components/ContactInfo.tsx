6745f33469cf96cd02bd6c37852847c2311855fe
niamatmarjan <86093827+NimBuzz01@users.noreply.github.com>
2024-05-29 13:40:21 +0530

    refactor tailwind classes

--- app/contact/_components/ContactInfo.tsx
+++ app/contact/_components/ContactInfo.tsx
@@ -6,21 +6,30 @@ import React from "react";
 const ContactInfo = () => {
   return (
     <div className="flex flex-col gap-10">
-      <div className="text-sm sm:text-base md:text-lg flex flex-col gap-1">
-        <p className="font-bold mb-5">Let&apos;s Talk</p>
+      <div className=" flex flex-col gap-1">
+        <p className="font-bold mb-5 text-sm sm:text-base md:text-lg">
+          Let&apos;s Talk
+        </p>
         <Typography text="hello@archiformagency" />
         <Typography text="+94 123 456 789" />
       </div>
-      <div className="text-sm sm:text-base md:text-lg flex flex-col gap-1">
-        <p className="font-bold mb-5">Visit</p>
+      <div className="flex flex-col gap-1">
+        <p className="font-bold mb-5 text-sm sm:text-base md:text-lg">Visit</p>
         <Typography text="123, Grand Avenue" />
         <Typography text="London, United Kingdom" />
       </div>
-      <div className="flex flex-col text-sm sm:text-base gap-2 md:text-lg">
-        <p className="font-bold mb-6">Socials</p>
+      <div className="flex flex-col  gap-2 ">
+        <p className="font-bold mb-6 text-sm sm:text-base md:text-lg">
+          Socials
+        </p>
         {socialLinks.map((link) => (
           <div key={link.title}>
-            <PageLink text={link.title} href={link.href} disableArrow />
+            <PageLink
+              text={link.title}
+              href={link.href}
+              disableArrow
+              className="font-medium"
+            />
           </div>
         ))}
       </div>
